13861347eccadff6ba46265afdf9ce599e75cae7
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-05-31 21:20:09 +0500

    login setup with google were setup

---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import { AiFillGoogleCircle } from "react-icons/ai";
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
+
+export default function Home() {
+  const { data: session } = useSession();
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.name} <br />
+        Signed in as {session.user.email} <br />
+        Signed in as <Image src={session.user.image} alt="img-user" width={80} height={80} className="rounded-lg"/> <br />
+        <button
+          onClick={() => signOut()}
+          className="bg-white p-2 px-4 rounded-lg hover:bg-black hover:text-white transition ease-in-out duration-1000"
+        >
+          <b className="flex items-center">
+            <AiFillGoogleCircle className="text-2xl font-bold mr-1" />
+            <b>Signout With Google</b>
+          </b>
+        </button>
+      </>
+    );
+  }
+  return (
+    <>
+      <Head>
+        <title>ecommerce-admin || Admin Login</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <div className="flex items-center bg-blue-900 h-screen w-screen">
+        <div className="text-center w-full">
+          <button
+            onClick={() => signIn('google')}
+            className="bg-white p-2 px-4 rounded-lg hover:bg-black hover:text-white transition ease-in-out duration-1000"
+          >
+            <b className="flex items-center">
+              <AiFillGoogleCircle className="text-2xl font-bold mr-1" />
+              <b>Login With Google</b>
+            </b>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
