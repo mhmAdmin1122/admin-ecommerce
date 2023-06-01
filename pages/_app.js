cedfbef7f53fa164e8df529bf40f28a9b75bb37e
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-06-01 12:00:09 +0500

    mongo server aded

--- pages/_app.js
+++ pages/_app.js
@@ -11,9 +11,3 @@ export default function App({
     </SessionProvider>
   );
 }
-
-// import type { AppProps } from 'next/app'
-
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
-// }
